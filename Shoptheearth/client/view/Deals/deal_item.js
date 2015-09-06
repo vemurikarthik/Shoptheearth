45cdf95b000d9269a237bb4fd5712953e43bb83c
vemurikarthik <f2011652@hyderabad.bits-pilani.ac.in>
2015-09-06 06:48:37 +0530

    Added Missing Changes

--- Shoptheearth/client/view/Deals/deal_item.js
+++ Shoptheearth/client/view/Deals/deal_item.js
@@ -1,4 +1,30 @@
+Meteor.startup(function() {  
+  GoogleMaps.load();
+});
+
+
+Template.postItem.events({
+
+   
+ 
+        
+        'click #but': function() {
 
 
 
+        
+             var la = Geolocation.latLng();
+        	 var distance = nearByLocation.getDistance({
+                    latA: la.lat,
+                    latB: Posts.findOne({_id:this._id}).lat,
+                    lngA:  la.lng,
+                    lngB: Posts.findOne({_id:this._id}).lng
+                    })  
 
+
+        	 
+         }
+
+
+
+});
